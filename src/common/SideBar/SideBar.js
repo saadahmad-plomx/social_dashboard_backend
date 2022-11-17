ecaf9c43be0081adda3a96cc0ce85579ffc6d80e
faiq <faiqwaheed94@gmail.com>
2022-11-17 16:37:34 +0500

    side bar with toggle

--- src/common/SideBar/SideBar.js
+++ src/common/SideBar/SideBar.js
@@ -1,170 +1,196 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import "./SideBar.css";
 
-import Footer from '../Footer/Footer'
-
-
-const drawerWidth = 240;
-
-function ResponsiveDrawer(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+const SideBar = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
+    <>
+      <nav
+        id="navbar_position"
+        class="navbar navbar-expand-md navbar-dark bg-secondary"
       >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler me-2"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasExample"
+            aria-controls="offcanvasExample"
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-        <Footer/>
-      </Box>
-    </Box>
-  );
-}
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
+          <a class="navbar-brand fw-bold me-auto ms-1 text-white" href="#">
+            {" "}
+            <span>
+              <img src="./assets/images/stars_logo.png" alt="" />
+            </span>{" "}
+            Stars Academy
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul id="navbar_buttons" class="navbar-nav mb-lg-0 ms-auto">
+              <li class="me-1 mb-1 mt-1">
+                <button class="btn btn-primary">Talent Hunt</button>
+              </li>
+              <li class="me-1 mb-1 mt-1">
+                <button class="btn btn-primary">Fee Structure</button>
+              </li>
+              <li class="me-1 mb-1 mt-1">
+                <button class="btn btn-primary">Results</button>
+              </li>
+              <li class="me-1 mb-1 mt-1">
+                <button class="btn btn-primary">Apply Online</button>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i class="bi bi-person-fill"></i>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Our Classes
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Our Courses
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Fee Structure
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div
+        class="offcanvas offcanvas-start bg-secondary text-white"
+        tabindex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div id="scrollbar_side" class="offcanvas-body">
+          <nav class="nav-dark">
+            <div id="sidebar_brand">
+              <span class="brand">CT</span>
+              <span class="brand_name">Creative Team</span>
+            </div>
+            <ul class="navbar-nav">
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Icons</span>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Maps</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  class="nav-link px-3 sidebar-link"
+                  data-bs-toggle="collapse"
+                  href="#collapseExample_notifications"
+                  role="button"
+                  aria-expanded="false"
+                  aria-controls="collapseExample_notifications"
+                >
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Notifications</span>
+                  <span id="right-icon" class="ms-auto">
+                    <i class="bi bi-chevron-down"></i>
+                  </span>
+                </a>
+                <div class="collapse" id="collapseExample_notifications">
+                  <ul class="navbar-nav ps-3">
+                    <li>
+                      <a href="#" class="nav-link px-3">
+                        <span class="me-2">
+                          <i class="bi bi-person-lines-fill"></i>
+                        </span>
+                        <span>Overview</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" class="nav-link px-3">
+                        <span class="me-2">
+                          <i class="bi bi-cpu"></i>
+                        </span>
+                        <span>Projects</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>User Profile</span>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Table List</span>
+                </a>
+              </li>
+              <li>
+                <a class="nav-link px-3 sidebar-link" role="button">
+                  <span class="me-3">
+                    <i class="bi bi-google-play"></i>
+                  </span>
+                  <span>Typography</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default ResponsiveDrawer;
+export default SideBar;
